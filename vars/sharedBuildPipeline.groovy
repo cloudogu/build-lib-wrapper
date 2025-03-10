4a6300a7ab449c80ad1d6d185fa4008b3ad848dc
Dennis Schwarzer <dennis.schwarzer@cloudogu.com>
2025-03-10 14:29:24 +0100

    lib referencing

--- vars/sharedBuildPipeline.groovy
+++ vars/sharedBuildPipeline.groovy
@@ -1,8 +1,4 @@
 #!groovy sharedBuildPipeline.groovy
-@Library([
-  'github.com/cloudogu/ces-build-lib@4.1.0',
-  'github.com/cloudogu/dogu-build-lib@v3.0.0'
-]) _
 import com.cloudogu.ces.cesbuildlib.*
 import com.cloudogu.ces.dogubuildlib.*
 
